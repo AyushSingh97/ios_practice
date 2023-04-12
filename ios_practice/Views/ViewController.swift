60746d1cc654fd1b856ffaac34c931344cd0553f
Ayush Singh <ayush.singh@nana.sa>
2023-04-12 10:49:12 +0530

    Added DFD diagram

--- ios_practice/Views/ViewController.swift
+++ ios_practice/Views/ViewController.swift
@@ -1,53 +1,60 @@
-//
-//  ViewController.swift
-//  ios_practice
-//
-//  Created by Unthinkable-mac-0050 on 28/03/23.
-//
-
 import UIKit
 
+/// This is the main view controller responsible for showing the UI and reacting to user interactions
 class ViewController: UIViewController, ViewDelegate {
-    
+    /// The presenter that handles the business logic and communicates with the data source
     private let presenter = Presenter(service: Service())
+
+    /// Toggle the background color of the view
+    /// - Parameter color: The new color to set the background view to
     func toggleBgColor(color: UIColor) {
         backgroundView.backgroundColor = color
     }
-    
+
+    /// Toggle the text of the label
+    /// - Parameter title: The new title to set the label to
     func toggleLabel(title: String) {
         label.text = title
     }
-    
 
+    /// The label that displays the text
     @IBOutlet weak var label: UILabel!
+
+    /// The view that represents the background of the app
     @IBOutlet weak var backgroundView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Set the view delegate for the presenter
         presenter.setViewDelegate(viewDelegate: self)
     }
 
+    /// Triggered when the toggle button is pressed
     @IBAction func onToggle(_ sender: UIButton) {
+        // Toggle the background color and label text using the presenter
         presenter.toggleColor(colorName: backgroundView.backgroundColor == UIColor.red ? "RED": "PURPLE")
         presenter.toggleLabel(text: label.text!)
-//        _toggleColor()
-//        _toggleLabel()
     }
-    
-//    func _toggleColor(){
-//        if(backgroundView.backgroundColor == UIColor.purple){
-//            backgroundView.backgroundColor = UIColor.red
-//            return
-//        }
-//        backgroundView.backgroundColor = UIColor.purple
-//        return
-//    }
-//    func _toggleLabel(){
-//        if(label.text?.uppercased() == "PURPLE"){
-//            label.text = "RED"
-//            return
-//        }
-//        label.text = "PURPLE"
-//        return
-//    }
-}
 
+    /*
+    /// Toggles the color of the background view between purple and red
+    func _toggleColor(){
+        if(backgroundView.backgroundColor == UIColor.purple){
+            backgroundView.backgroundColor = UIColor.red
+            return
+        }
+        backgroundView.backgroundColor = UIColor.purple
+        return
+    }
+
+    /// Toggles the text of the label between purple and red
+    func _toggleLabel(){
+        if(label.text?.uppercased() == "PURPLE"){
+            label.text = "RED"
+            return
+        }
+        label.text = "PURPLE"
+        return
+    }
+    */
+}
