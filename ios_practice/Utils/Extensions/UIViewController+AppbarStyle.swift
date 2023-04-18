50cd9b6fa028e8969f0bda6002db1db54e0ec486
Ayush Singh <ayush.singh@nana.sa>
2023-04-18 12:34:08 +0530

    minor fixes

--- ios_practice/Utils/Extensions/UIViewController+AppbarStyle.swift
+++ ios_practice/Utils/Extensions/UIViewController+AppbarStyle.swift
@@ -1,13 +1,13 @@
 import UIKit
 extension UIViewController{
     func setAppbarStyle(_ appbar: UINavigationItem!) {
-        let label = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: view.frame.size.width, height: 36))
+        let label = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: view.frame.size.width, height: 30))
         label.font = UIFont.boldSystemFont(ofSize: 24.0)
         label.numberOfLines = 2
         label.backgroundColor = .clear
         label.textAlignment = .left
         label.textColor = .white
-        label.shadowColor = .black
+        label.shadowColor = .clear
         label.text = "Global News"
         appbar.titleView = label
     }
